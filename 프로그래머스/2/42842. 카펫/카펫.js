f7ffc967960387b6d0e7bbbf6bfb24a27094bd6a
ClownNero <110890911+ClownNero@users.noreply.github.com>
2024-03-21 15:55:51 +0900

    Update 카펫.js

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -2,7 +2,7 @@ const solution = (brown, yellow) => {
     const answer = [];
     for (let x = 3; x <= (brown+yellow)/x; x++) {
         let y = Math.floor((brown+yellow)/x);
-        if( (x-2) * (y-2) === yellow) {
+        if( (x-2) * (y-2) === yellow) { // 가로(y), 세로(x)
             answer.push(y,x);
             break;
         }
