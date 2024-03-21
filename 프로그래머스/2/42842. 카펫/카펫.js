abe93693bace13f131fff8d4f87a2182347849ca
ClownNero <110890911+ClownNero@users.noreply.github.com>
2024-03-21 15:51:35 +0900

    [level 2] Title: 카펫, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -0,0 +1,12 @@
+const solution = (brown, yellow) => {
+    const answer = [];
+    for (let x = 3; x <= (brown+yellow)/x; x++) {
+        let y = Math.floor((brown+yellow)/x);
+        if( (x-2)*(y-2)=== yellow) {
+            answer.push(y,x);
+            break;
+        }
+    }
+ 
+    return answer; 
+}
