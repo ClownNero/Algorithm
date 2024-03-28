f31d04401c4a168ee0c22bf3d895e0b90032f9b3
ClownNero <110890911+ClownNero@users.noreply.github.com>
2024-03-29 00:22:18 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.js
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.js
@@ -1,7 +1,7 @@
 function solution(money) {
     const coffee = 5500;
     const cup = Math.floor(money / coffee);
-    const remainder = money - coffee * cup;
+    const remainder = money % coffee;
     let answer = [cup, remainder];
     
     return answer;
