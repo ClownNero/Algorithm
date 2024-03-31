69fab177b68cbf1b694139a258785b754007ff48
ClownNero <110890911+ClownNero@users.noreply.github.com>
2024-03-31 15:14:16 +0900

    [level 0] Title: 공 던지기, Time: 0.02 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120843. 공 던지기/공 던지기.js
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.js
@@ -1,8 +1,3 @@
 function solution(numbers, k) {
-    let answer = 0;
-    const len = numbers.length;
-    for(let i =0; i<k; i++){
-        answer = numbers[2*i%len];
-    }
-    return answer;
+    return numbers[2*(k-1)%numbers.length];
 }
