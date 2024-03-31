9ca02f18c205cfeef0429bf9f4b205b915a2b50d
ClownNero <110890911+ClownNero@users.noreply.github.com>
2024-03-31 15:01:43 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.js
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -1,12 +1,9 @@
 function solution(num_list, n) {
     let answer = [];
-    for(let i =0; i<num_list.length;){
-        let array = [];
-        for(let j =0; j<n; j++){
-            array.push(num_list[i+j]);
-        }
-        answer.push(array);
-        i+=n;
+
+    while(num_list.length) {
+        answer.push(num_list.splice(0,n));
     }
+
     return answer;
 }
