ad35c85ce60e52cbf1a0b3d6b08623c2880f8be0
ClownNero <110890911+ClownNero@users.noreply.github.com>
2024-03-31 14:24:49 +0900

    [level 0] Title: 개미 군단, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.js
@@ -1,8 +1,6 @@
 function solution(hp) {
-    let dp = Array(1000).fill(0);
-    dp[1] = 1;dp[2] = 2; dp[3]=1; dp[4]=2; 
-    for(let i=5; i<=1000; i++){
-        dp[i] = Math.min(dp[i-1]+1, Math.min(dp[i-3] + 1,dp[i-5]+1));
-    }
-    return dp[hp];
+    const 장군개미 = Math.floor(hp / 5);
+    const 병정개미 = Math.floor((hp - (장군개미 * 5)) / 3);
+    const 일개미 = hp - ((장군개미 * 5) + (병정개미 * 3));
+    return 장군개미+병정개미+일개미;
 }
