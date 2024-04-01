8dc789abadae32ead464d85a3681ba0420a3f75b
ClownNero <110890911+ClownNero@users.noreply.github.com>
2024-04-01 13:45:31 +0900

    [level 0] Title: 주사위의 개수, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.js
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.js
@@ -1,5 +1,6 @@
 function solution(box, n) {
-    let answer = 0;
-    answer = Math.floor(box[0]/n) * Math.floor(box[1]/n)*Math.floor(box[2]/n);
-    return answer;
+    let [width, length, height] = box;
+
+    return Math.floor(width / n) * Math.floor(length / n) * Math.floor(height / n);
+
 }
