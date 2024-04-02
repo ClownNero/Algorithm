0965629a44b54f42df3fa56b0964ae5702ab76db
ClownNero <110890911+ClownNero@users.noreply.github.com>
2024-04-02 15:48:44 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.js
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.js
@@ -1,11 +1,3 @@
 function solution(my_string) {
-    var answer = '';
-    const set = new Set();
-    for(let i of my_string){
-        set.add(i);
-    }
-    for(let i of set){
-        answer+=i;
-    }
-    return answer;
+    return [...new Set(my_string)].join('');
 }
