f138167281df1da9430ef08a2804eb430432aa66
ClownNero <110890911+ClownNero@users.noreply.github.com>
2024-04-03 18:49:43 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -1,7 +1,5 @@
 function solution(my_string, num1, num2) {
-    const array = [...my_string];
-    const a = array[num1];
-    const b = array[num2];
-    array[num1] = b; array[num2] = a;
-    return array.join('');
+    my_string = my_string.split('');
+    [my_string[num1], my_string[num2]] = [my_string[num2], my_string[num1]];
+    return my_string.join('');
 }
