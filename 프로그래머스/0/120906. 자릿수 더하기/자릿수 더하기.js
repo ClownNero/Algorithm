23bb3f9209e8ecb3a4e4cb64ed88d56f44cdbb92
ClownNero <110890911+ClownNero@users.noreply.github.com>
2024-04-04 18:03:19 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.js
@@ -1,9 +1,8 @@
 function solution(n) {
     let answer = 0;
     while(n > 0){
-        const num = n % 10;
+        answer += n % 10;
         n = parseInt(n/10);
-        answer +=num;
     }
     return answer;
 }
