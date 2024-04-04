aeb74c45b982ee247b5ad47d34bfaf28fa13f450
ClownNero <110890911+ClownNero@users.noreply.github.com>
2024-04-04 18:12:22 +0900

    [level 0] Title: OX퀴즈, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.js
@@ -0,0 +1,9 @@
+function solution(quiz) {
+    return quiz.map((v)=>{
+        const [calc, result] = v.split(' = ');
+        const sign = calc.includes('+') ? 1 : -1
+        const [a, b] = calc.split(sign === 1 ? ' + ' : ' - ');
+
+        return +a + (+b * sign) === +result ? 'O' : 'X'
+    });
+}
