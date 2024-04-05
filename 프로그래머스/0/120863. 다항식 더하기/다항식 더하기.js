1f86d96b07fbc6dbc5b565fcd52ee265bf0ef3ae
ClownNero <110890911+ClownNero@users.noreply.github.com>
2024-04-05 17:17:50 +0900

    [level 0] Title: 다항식 더하기, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.js
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.js
@@ -1,19 +1,16 @@
 function solution(polynomial) {
-    let count = 0;
-    let num =0;
-    let answer ='';
-    const array = polynomial.split(" + ");
-    for(let i of array){
-         if(i.includes('x')){
-            const num = i.split('x');
-            if(num[0] === '') count++;
-            else count +=+num[0];
-        } else num+=+i;
-    }
-    if(count > 1 && num > 0 ) answer = `${count}x + ${num}`;
-    else if(count ===1 && num > 0) answer = `x + ${num}`;
-    else if(count > 1 && num === 0) answer = `${count}x`;
-    else if(count === 1 && num === 0) answer = `x`;
-    else if(count === 0 && num > 0) answer = `${num}`;
-    return answer;
+    const arr = polynomial.split(" + ");
+    const xNum = arr
+                .filter(n => n.includes("x"))
+                .map(n => n.replace('x', '') || '1')
+                .reduce((acc, cur) => acc + parseInt(cur, 10), 0);
+    const num = arr
+                .filter(n => !isNaN(n))
+                .reduce((acc, cur) => acc + parseInt(cur, 10), 0);
+
+    let answer = [];
+    if(xNum) answer.push(`${xNum === 1 ? "" : xNum}x`);
+    if(num) answer.push(num);
+
+    return answer.join(" + ");
 }
