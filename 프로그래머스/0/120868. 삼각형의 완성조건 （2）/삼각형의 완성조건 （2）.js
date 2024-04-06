6c50b24263d89a2b06b2de32be95ebb6345a5d4c
ClownNero <110890911+ClownNero@users.noreply.github.com>
2024-04-06 15:51:13 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.js
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.js
@@ -1,18 +1,13 @@
 function solution(sides) {
-    // 가장 긴 변의 길이는 다른 두 변의 길이의 합보다 작아야 합니다.
-    let answer = 0;
-    let [a, b] = sides;
-    let c = 0;
-    const max = Math.max(a,b);
-    const min = Math.min(a,b);
-    // 주어진 두 길이 중 하나가 가장 길 때,
-    for(let i = max; max < i+min ; i--){
-        answer ++;
-    }
-    // 주어진 두 길이 외 나머지 하나가 가장 길 때,
-    for(let i =a+b-1; i<a+b && i> max; i--){
-        answer++;
+    let a = Math.min(...sides)
+    let b = Math.max(...sides)
+    let answer = 0
+    for(let i = 1 ; i < a+b ; i++){
+        let arr = [a,b,i].sort((a,b) => a-b)
+        let [q,w,e] = arr
+        if(q+w > e){
+            answer++
+        }
     }
     return answer;
-
 }
