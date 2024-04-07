5eba1fe0e7adc59ed2e198711d211fb840880e64
ClownNero <110890911+ClownNero@users.noreply.github.com>
2024-04-07 18:16:44 +0900

    [level 0] Title: A로 B 만들기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.js
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.js
@@ -1,12 +1,3 @@
 function solution(before, after) {
-    const len = before.length;
-    const barray = [...before];
-    const aarray = [...after];
-    return barray.filter((v)=>{
-       if (aarray.indexOf(v) !== -1) {
-           aarray[aarray.indexOf(v)] = 1;
-            return true;
-       }else
-           return false;
-    }).length === len ? 1 : 0;
+    return before.split('').sort().join('') === after.split('').sort().join('') ? 1 : 0;
 }
