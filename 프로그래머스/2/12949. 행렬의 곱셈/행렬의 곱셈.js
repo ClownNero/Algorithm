a8fa388982813ac68880b9095da08a2e3cea6b44
ClownNero <110890911+ClownNero@users.noreply.github.com>
2024-04-12 19:15:22 +0900

    [level 2] Title: 행렬의 곱셈, Time: 3.02 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
@@ -1,15 +1,15 @@
 function solution(arr1, arr2) {
-    let answer = [];
+    const answer = [];
     for(let i =0; i<arr1.length; i++){
-        let result = [];
+        const newArr = [];
         for(let j=0; j<arr2[0].length; j++){
-            let a=0;
+            let num=0;
             for(let k = 0; k< arr2.length; k++){
-                a+= arr1[i][k] * arr2[k][j];
+                num+= arr1[i][k] * arr2[k][j];
             }
-            result.push(a);
+            newArr.push(num);
         }
-        answer.push(result);
+        answer.push(newArr);
     }
     return answer;
 }
