24c4145f93bf2d7247ccd74846164bf1eb2b929c
ClownNero <110890911+ClownNero@users.noreply.github.com>
2024-04-14 20:54:03 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.js
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.js
@@ -1,17 +1,9 @@
 function isPrime(num) {
-  if(num === 0 || num === 1){
-    return false;
-  }
-  if(num === 2) {
-    return true;
-  }
- 
+  if(num === 0 || num === 1) return false; 
   for(let i = 2; i <= Math.floor(Math.sqrt(num)); i++){
-    if(num % i === 0){      
-      return false; 
-    }
+    if(num % i === 0) return false; 
   }
-  return true; 
+  return true;
 }
 
 function solution(n, k) {
