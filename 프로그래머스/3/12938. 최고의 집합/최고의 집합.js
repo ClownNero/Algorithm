d9d2919ae0a2a2e1ed9e6e02ade492effc96b871
ClownNero <110890911+ClownNero@users.noreply.github.com>
2024-04-23 17:00:00 +0900

    [level 3] Title: 최고의 집합, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.js
@@ -0,0 +1,10 @@
+function solution(n, s) {
+    if (n > s) return [-1];
+    const mid = Math.floor(s / n);
+    const answer = new Array(n).fill(mid);
+    
+    for (let i = 0; i < s % n; i++){
+        answer[n - 1 - i]++;
+    }
+    return answer;
+}
