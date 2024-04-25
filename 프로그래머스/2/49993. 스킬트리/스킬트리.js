663a63d915e893f10953d3c38989e8b33670bbc5
ClownNero <110890911+ClownNero@users.noreply.github.com>
2024-04-25 17:41:36 +0900

    [level 2] Title: 스킬트리, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.js
@@ -0,0 +1,12 @@
+function solution(skill, skill_trees) {
+    const newArr = skill.split("");
+    let count = 0;
+    for(let i = 0; i < skill_trees.length; i++) {
+        let str = skill_trees[i].split("").filter(word => newArr.includes(word)).join("");
+        
+        if(str === skill.substring(0,str.length)) {
+            count++;
+        }
+    }
+    return count;
+}
