693ae2af1610a2e91de56ba253ac20364b967d65
ClownNero <110890911+ClownNero@users.noreply.github.com>
2024-04-29 20:00:01 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 1.80 ms, Memory: 34.4 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
@@ -1,20 +1,16 @@
 function solution(files) {
-    let answerWrap = files.map((file, index)=> ({file, index}));
-    const compare = (a, b) => {
-        const reg = /(\D*)([0-9]*)/i;
-        const A = a.match(reg);
-        const B = b.match(reg);
-        const compareHead = A[1].toLowerCase().localeCompare(B[1].toLowerCase());
-        const compareNumber = (a, b) => {
-            return parseInt(a) > parseInt(b)?
-                1: parseInt(a) < parseInt(b) ?
-                -1 : 0
-        }
-        return compareHead === 0 ? compareNumber(A[2], B[2]) : compareHead
-    }
-    answerWrap.sort((a, b) => {
-        const result = compare(a.file, b.file);
-        return result === 0 ? a.index - b.index: result;
+    const re = /^([a-zA-Z-\. ]+)([0-9]+)(.*)$/
+    let dict = []
+    files.forEach((entry, idx) => {
+        let [fn, head, num] = entry.match(re)
+        dict.push({fn, head: head.toLowerCase(), num: parseInt(num), idx})
     })
-    return answerWrap.map(answer => answer.file);
+
+    return dict.sort((a, b) => {
+        if (a.head > b.head) return  1
+        if (a.head < b.head) return -1
+        if (a.num > b.num) return  1
+        if (a.num < b.num) return -1
+        return a.idx - b.idx
+    }).map(e => e.fn)
 }
