d9b48c8d1b32e3fab3785c65a5acce4069da94bd
ClownNero <110890911+ClownNero@users.noreply.github.com>
2024-04-29 20:03:45 +0900

    [level 2] Title: 오픈채팅방, Time: 148.12 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
@@ -1,17 +1,17 @@
 function solution(record) {
-    let answer = []
-    const userInfo = new Map()
+    let answer = [];
+    const userInfo = new Map();
 
     record.forEach(entry => {
-        let [command, uid, nick] = entry.split(' ')
-        if (command === 'Enter' || command === 'Change') userInfo.set(uid, nick)
+        let [command, uid, nick] = entry.split(' ');
+        if (command === 'Enter' || command === 'Change') userInfo.set(uid, nick);
     })
 
     record.forEach(entry => {
-        let [command, uid] = entry.split(' ')
-        if (command === 'Enter') answer.push(`${userInfo.get(uid)}님이 들어왔습니다.`)
-        if (command === 'Leave') answer.push(`${userInfo.get(uid)}님이 나갔습니다.`)
+        let [command, uid] = entry.split(' ');
+        if (command === 'Enter') answer.push(`${userInfo.get(uid)}님이 들어왔습니다.`);
+        if (command === 'Leave') answer.push(`${userInfo.get(uid)}님이 나갔습니다.`);
     })
 
-    return answer
+    return answer;
 }
