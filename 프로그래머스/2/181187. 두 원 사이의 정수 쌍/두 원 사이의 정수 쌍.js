03d6f7826d51ad3ff87a6f08e81f528fc4a1cd26
ClownNero <110890911+ClownNero@users.noreply.github.com>
2024-05-31 20:35:06 +0900

    [level 2] Title: 두 원 사이의 정수 쌍, Time: 12.45 ms, Memory: 37.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.js
@@ -0,0 +1,10 @@
+function solution(r1, r2) {
+    let answer = 0;
+    for(let x = 1;x<=r2;x++){
+        let maxY = Math.floor(Math.sqrt(r2**2 - x**2));
+        let minY = r1>x ? Math.ceil(Math.sqrt(r1**2 - x**2)): 0;
+        answer+= (maxY - minY + 1);
+    }
+    answer *= 4;
+    return answer;
+}
