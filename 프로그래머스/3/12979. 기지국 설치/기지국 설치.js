4f188cf980237ae72555cc13b54dd87298619643
ClownNero <110890911+ClownNero@users.noreply.github.com>
2024-06-10 17:26:22 +0900

    [level 3] Title: 기지국 설치, Time: 1.45 ms, Memory: 34.3 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.js
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.js
@@ -1,17 +1,18 @@
 function solution(n, stations, w) {
     let answer = 0;
-    let idx = 0;
     let start = 1;
+    const coverage = [];
+    const range = w*2+1;
     
-    while(start <= n){
-        if(start >= stations[idx] - w && start <= stations[idx] + w){
-            start = stations[idx] + w;
-            idx++;
-        } else{
-            start +=2 * w;
-            answer++;
+    for(let station of stations){
+        if(station - w - start > 0){
+            coverage.push(station-w-start);
         }
-        start++;
+        start = station + w + 1;
+    }
+    if(start <= n) coverage.push(n + 1 - start);
+    for(let num of coverage){
+        answer += Math.ceil(num/range);
     }
     return answer;
 }
